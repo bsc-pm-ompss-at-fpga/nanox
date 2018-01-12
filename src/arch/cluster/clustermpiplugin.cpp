bsc-pm-ompss-at-fpga/nanox

commit cf28d062a446e6e4facd5f239407897ae5b266cd

Add nodeId argument in getRemoteWorkDescriptor

src/arch/cluster/clustermpiplugin.cpp
@@ -25,7 +25,6 @@
 #include "remoteworkdescriptor_decl.hpp"
 #include "basethread.hpp"
 #include "smpprocessor.hpp"
-#include "clusterthread_decl.hpp"
 #ifdef OpenCL_DEV
 #include "opencldd.hpp"
 #endif
@@ -145,10 +144,10 @@ int ClusterMPIPlugin::initNetwork(int *argc, char ***argv)
       }
       _gasnetApi->_rwgs = NEW GASNetAPI::ArchRWDs[ nodes ];
       for ( unsigned int idx = 0; idx < nodes; idx += 1 ) {
-         _gasnetApi->_rwgs[idx][0] = getRemoteWorkDescriptor(0);
-         _gasnetApi->_rwgs[idx][1] = getRemoteWorkDescriptor(1);
-         _gasnetApi->_rwgs[idx][2] = getRemoteWorkDescriptor(2);
-         _gasnetApi->_rwgs[idx][3] = getRemoteWorkDescriptor(3);
+         _gasnetApi->_rwgs[idx][0] = getRemoteWorkDescriptor(idx, 0);
+         _gasnetApi->_rwgs[idx][1] = getRemoteWorkDescriptor(idx, 1);
+         _gasnetApi->_rwgs[idx][2] = getRemoteWorkDescriptor(idx, 2);
+         _gasnetApi->_rwgs[idx][3] = getRemoteWorkDescriptor(idx, 3);
       }
       _clusterThread->addThreadsFromPEs( tmp_nodes.size(), &(tmp_nodes[0]) );
    }
@@ -192,8 +191,8 @@ System::CachePolicyType ClusterMPIPlugin::getCachePolicy ( void ) const {
    return _cachePolicy;
 }
 
-RemoteWorkDescriptor * ClusterMPIPlugin::getRemoteWorkDescriptor( int archId ) {
-   RemoteWorkDescriptor *rwd = NEW RemoteWorkDescriptor( archId );
+RemoteWorkDescriptor * ClusterMPIPlugin::getRemoteWorkDescriptor( int nodeId, int archId ) {
+   RemoteWorkDescriptor *rwd = NEW RemoteWorkDescriptor( nodeId );
    rwd->_mcontrol.preInit();
    rwd->_mcontrol.initialize( *_cpu );
    return rwd;

src/arch/cluster/clustermpiplugin_decl.hpp
@@ -60,7 +60,7 @@ class ClusterMPIPlugin : public ArchPlugin
       int getGpuPresend() const;
       int getSmpPresend() const;
       System::CachePolicyType getCachePolicy ( void ) const;
-      RemoteWorkDescriptor * getRemoteWorkDescriptor( int archId );
+      RemoteWorkDescriptor * getRemoteWorkDescriptor( unsigned int nodeId, int archId );
       bool getAllocFit() const;
       bool unalignedNodeMemory() const;
       virtual int initNetwork(int *argc, char ***argv);
@@ -71,7 +71,7 @@ class ClusterMPIPlugin : public ArchPlugin
       virtual void finalize();
 
       virtual ProcessingElement * createPE( unsigned id, unsigned uid );
-      virtual unsigned getNumThreads() const; 
+      virtual unsigned getNumThreads() const;
       void addPEs( PEMap &pes ) const;
       virtual void addDevices( DeviceList &devices ) const {}
       virtual unsigned int getNumPEs() const;

src/arch/cluster/clusterplugin.cpp
@@ -180,8 +180,8 @@ System::CachePolicyType ClusterPlugin::getCachePolicy ( void ) const {
    return _cachePolicy;
 }
 
-RemoteWorkDescriptor * ClusterPlugin::getRemoteWorkDescriptor( int archId ) {
-   RemoteWorkDescriptor *rwd = NEW RemoteWorkDescriptor( archId );
+RemoteWorkDescriptor * ClusterPlugin::getRemoteWorkDescriptor( unsigned int nodeId, int archId ) {
+   RemoteWorkDescriptor *rwd = NEW RemoteWorkDescriptor( nodeId );
    rwd->_mcontrol.preInit();
    rwd->_mcontrol.initialize( *_cpu );
    return rwd;
@@ -262,10 +262,10 @@ void ClusterPlugin::startSupportThreads() {
          }
 
          _gasnetApi->_rwgs = (GASNetAPI::ArchRWDs *) NEW GASNetAPI::ArchRWDs();
-         _gasnetApi->_rwgs[0][0] = getRemoteWorkDescriptor(0);
-         _gasnetApi->_rwgs[0][1] = getRemoteWorkDescriptor(1);
-         _gasnetApi->_rwgs[0][2] = getRemoteWorkDescriptor(2);
-         _gasnetApi->_rwgs[0][3] = getRemoteWorkDescriptor(3);
+         _gasnetApi->_rwgs[0][0] = getRemoteWorkDescriptor(0, 0);
+         _gasnetApi->_rwgs[0][1] = getRemoteWorkDescriptor(0, 1);
+         _gasnetApi->_rwgs[0][2] = getRemoteWorkDescriptor(0, 2);
+         _gasnetApi->_rwgs[0][3] = getRemoteWorkDescriptor(0, 3);
       }
    }
 }

src/arch/cluster/clusterplugin_decl.hpp
@@ -59,7 +59,7 @@ class ClusterPlugin : public ArchPlugin
       int getGpuPresend() const;
       int getSmpPresend() const;
       System::CachePolicyType getCachePolicy ( void ) const;
-      RemoteWorkDescriptor * getRemoteWorkDescriptor( int archId );
+      RemoteWorkDescriptor * getRemoteWorkDescriptor( unsigned int nodeId, int archId );
       bool getAllocFit() const;
       bool unalignedNodeMemory() const;
 
@@ -68,7 +68,7 @@ class ClusterPlugin : public ArchPlugin
       virtual void finalize();
 
       virtual ProcessingElement * createPE( unsigned id, unsigned uid );
-      virtual unsigned getNumThreads() const; 
+      virtual unsigned getNumThreads() const;
       void addPEs( PEMap &pes ) const;
       virtual void addDevices( DeviceList &devices ) const {}
       virtual unsigned int getNumPEs() const;

src/arch/cluster/remoteworkdescriptor_decl.hpp
@@ -28,7 +28,7 @@ namespace nanos {
          unsigned int _destinationNode;
       public:
          RemoteWorkDescriptor();
-         RemoteWorkDescriptor(unsigned int destinatioNode);
+         RemoteWorkDescriptor( unsigned int destinatioNode );
          virtual void exitWork( WorkDescriptor &work );
    };
 

commit 142384373c3a615b9b93358e259a7e6bc14fe3a9

Add missing include in clustermpiplugin.cpp
Commit cf28d062a446e6e4facd5f239407897ae5b266cd accidentally removed a needed include

src/arch/cluster/clustermpiplugin.cpp
@@ -25,6 +25,7 @@
 #include "remoteworkdescriptor_decl.hpp"
 #include "basethread.hpp"
 #include "smpprocessor.hpp"
+#include "clusterthread_decl.hpp"
 #ifdef OpenCL_DEV
 #include "opencldd.hpp"
 #endif