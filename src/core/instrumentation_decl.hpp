1cea8e9dfb60325d27a93116cb3252f4fa34ca88
Antonio Filgueras <antonio.filgueras@bsc.es>
2018-02-22 17:58:16 +0100

    Instrument cluster task offload

--- src/core/instrumentation_decl.hpp
+++ src/core/instrumentation_decl.hpp
@@ -680,6 +680,9 @@ namespace nanos {
             /* 73 */ registerEventKey("fpga-finish-task", "Finished FPGA task ID that is being processed", true, EVENT_ADVANCED);
             /* 74 */ registerEventKey("fpga-run-tasks", "Number of FPGA tasks submitted to the accels", true, EVENT_ADVANCED);
 
+            /* 75 */ registerEventKey("cluster-offload-task", "Task being offloaded to a remote node", EVENT_DEVELOPER);
+            /* 76 */ registerEventKey("cluster-select-node", "Remote node being selected for remote task offload", EVENT_DEVELOPER);
+
             /* ** */ registerEventKey("debug","Debug Key", true, EVENT_ADVANCED ); /* Keep this key as the last one */
          }
 
