bsc-pm-ompss-at-fpga/nanox

commit c44d828a687cde31b5503a912f2d8b5352cdc813

Add support for device events in extrae instrumentation
As a side effect, the OMPT instrumentation changes its internal interface inside Nanos++ and seems that paraver traces are not showing the device events

src/plugins/instrumentation/ompt.cpp
@@ -486,9 +486,7 @@ namespace nanos
       private:
          ompt_task_id_t * _previousTask;
          int            * _threadActive;
-         std::vector < DeviceInstrumentation * > _devices;
-         int            _deviceCount;
-         int            _numThreads;
+         int              _numThreads;
 
          //target buffer callbacks
          //TODO these should be inside a vector, per accelerator
@@ -509,10 +507,32 @@ namespace nanos
 
          std::vector < BufferInfo* > _devEventBuffers;
 
+         //! \brief Translates the nanos_event_type_t to ompt_event_t
+         static ompt_event_t getOMPTEventType( nanos_event_type_t type ) {
+            ompt_event_t ret = ( ompt_event_t )( 0 );
+            switch ( type ) {
+               case NANOS_STATE_START:
+               case NANOS_STATE_END:
+               case NANOS_SUBSTATE_START:
+               case NANOS_SUBSTATE_END:
+               case NANOS_PTP_START:
+               case NANOS_PTP_END:
+               case NANOS_POINT:
+                  break; //< Previous event types are not supported yet
+               case NANOS_BURST_START:
+                  ret = ompt_event_task_begin;
+                  break;
+               case NANOS_BURST_END:
+                  ret = ompt_event_task_end;
+                  break;
+               default: break;
+            }
+            return ret;
+         }
 
       public:
          InstrumentationOMPT( ) : Instrumentation( *NEW InstrumentationContextDisabled()),
-            _previousTask( NULL ), _threadActive( NULL ), _deviceCount( 0 ), _requestBufferCallback( NULL ),
+            _previousTask( NULL ), _threadActive( NULL ), _requestBufferCallback( NULL ),
             _completeBufferCallback( NULL ) {}
          ~InstrumentationOMPT() {
             for ( std::vector< BufferInfo* >::iterator it = _devEventBuffers.begin();
@@ -531,7 +551,7 @@ namespace nanos
 
          void initialize( void )
          {
-            ompt_initialize ( ompt_nanos_lookup, "Nanos++ 0.8a", 1);
+            ompt_initialize ( ompt_nanos_lookup, "Nanos++ 0.15a", 1);
             int nthreads = sys.getSMPPlugin()->getNumThreads();
             _numThreads = nthreads;
             _previousTask = ( ompt_task_id_t *) malloc ( nthreads * sizeof(ompt_task_id_t) );
@@ -803,13 +823,6 @@ namespace nanos
             _threadActive = ( int* ) realloc( _threadActive, _numThreads*sizeof( int ) );
          }
 
-         virtual void registerInstrumentDevice( DeviceInstrumentation *devInstr ) {
-            _devices.push_back( devInstr );
-            devInstr->setId( _deviceCount++ );
-         }
-
-         int getNumDevices() { return _deviceCount; }
-
          void setRequestBufferCallback( ompt_target_buffer_request_callback_t callback )
          {
             _requestBufferCallback = callback;
@@ -822,7 +835,6 @@ namespace nanos
 
          DeviceInstrumentation *getDeviceInstrumentation( int id )
          {
-            //return _devices[ id ];
             return sys.getDeviceInstrumentation( id );
          }
 
@@ -852,75 +864,71 @@ namespace nanos
             }
          }
 
-         void addDeviceEvent( const DeviceEvent &event ) {
-            const DeviceInstrumentation *devInstr = event.getDeviceInstrumentation();
-            int deviceId = devInstr->getId();
+         virtual void addDeviceEventList( const DeviceInstrumentation& ctx, unsigned int count, DeviceEvent *events )
+         {
+            int deviceId = ctx.getId();
             BufferInfo &buffer = *_devEventBuffers[ deviceId ];
-            buffer.lock++; //Lock the buffer to get an empty slot
+            unsigned int eventIdx = 0; //< Index being handled of events array
 
-            //Request buffer if it's not initialized
-            if ( buffer.buffer == NULL ) {
-               //buffer request
-               _requestBufferCallback( (ompt_target_buffer_t**)&buffer.buffer, &buffer.size );
-               buffer.done = 0;
-               buffer.begin = 0;
-               buffer.current = 0;
-               buffer.records = buffer.size / sizeof( ompt_record_ompt_t );
+            while ( eventIdx < count ) {
+               buffer.lock++; //Lock the buffer to get an empty slot
+
+               //Request buffer if it's not initialized
+               if ( buffer.buffer == NULL ) {
+                  //buffer request
+                  _requestBufferCallback( (ompt_target_buffer_t**)&buffer.buffer, &buffer.size );
+                  buffer.done = 0;
+                  buffer.begin = 0;
+                  buffer.current = 0;
+                  buffer.records = buffer.size / sizeof( ompt_record_ompt_t );
+               }
+
+               //Call to complete if no more records fit in current buffer
+               if ( buffer.current == buffer.records ) {
+                  //Wait until last writes end
+                  while ( buffer.done.value() != buffer.records ) {}
+
+                  //There is no space to save the event -> call completion
+                  _completeBufferCallback( deviceId, ( ompt_target_buffer_t *) buffer.buffer,
+                     buffer.size, buffer.begin, buffer.current );
+                  //reset current pointer to recycle the buffer
+                  buffer.done = buffer.begin;
+                  buffer.current = buffer.begin;
+               }
+               unsigned int current = buffer.current; //< Initial position to write in the buffer
+               unsigned int countCurrent = std::min( count - eventIdx, buffer.records - current );
+               buffer.current += countCurrent;
+               buffer.lock--; //Release the lock and use local variable
+
+               for (unsigned int i = 0; i < countCurrent; ++i) {
+                  //add event to buffer
+                  ompt_record_ompt_t *omptEvent = &buffer.buffer[ current + i ];
+                  //Nanos device events are already conveniently set to match ompt spec
+                  omptEvent->type = ompt_event_task_switch; //getOMPTEventType( events[eventIdx].getType() );
+                  omptEvent->time = events[eventIdx].getDeviceTime();
+                  //Host thread that emits the event (TODO check)
+                  omptEvent->thread_id = ( ompt_thread_id_t )( getCurrentThreadId() );
+                  omptEvent->dev_task_id = events[eventIdx].getValue(); //< Event value should be the taskID
+
+                  switch ( events[eventIdx].getType() ) {
+                     case NANOS_BURST_START:
+                        omptEvent->record.task_switch.first_task_id = 0;
+                        omptEvent->record.task_switch.second_task_id = events[eventIdx].getValue();
+                        break;
+                     case NANOS_BURST_END:
+                        omptEvent->record.task_switch.first_task_id = events[eventIdx].getValue();
+                        omptEvent->record.task_switch.second_task_id = 0;
+                        break;
+                     default:
+                        warning( "Found an unsupported event type in InstrumentationOMPT::addDeviceEventList" );
+                        break;
+                  }
+
+                  eventIdx++;
+               }
+
+               buffer.done += countCurrent;
             }
-
-            //Call to complete if no more records fit in current buffer
-            if ( buffer.current == buffer.records ) {
-               //Wait until last writes end
-               while ( buffer.done.value() != buffer.records ) {}
-
-               //There is no space to save the event -> call completion
-               _completeBufferCallback( deviceId, ( ompt_target_buffer_t *) buffer.buffer,
-                       buffer.size,
-                     buffer.begin, buffer.current );
-               //reset current pointer to recycle the buffer
-               buffer.done = buffer.begin;
-               buffer.current = buffer.begin;
-            }
-            unsigned int current = buffer.current++;
-            buffer.lock--; //Release the lock and use local variable
-
-            //add event to buffer
-            ompt_record_ompt_t *omptEvent = &buffer.buffer[ current ];
-            //Nanos device events are already conveniently set to match ompt spec
-            omptEvent->type = ( ompt_event_t ) event.getEventType();
-            omptEvent->time = event.getDeviceTime();
-            //Host thread that emits the event (TODO check)
-            omptEvent->thread_id = (ompt_thread_id_t) getCurrentThreadId();
-            //Task ID (TODO check)
-            const WorkDescriptor * wd = event.getWD();
-            const WorkDescriptor * auxWD = event.getAuxWD();
-            //When running a task (task switch from 0 to a task) wd may be null
-            if ( wd != NULL ) {
-               omptEvent->dev_task_id = wd->getId();
-            } else {
-               omptEvent->dev_task_id = auxWD->getId();
-            }
-
-            switch ( event.getEventType() ) {
-               case TaskBegin:
-                  omptEvent->record.new_task.parent_task_id = wd->getParent()->getId();
-                  omptEvent->record.new_task.parent_task_frame = NULL;
-                  omptEvent->record.new_task.new_task_id = wd->getId();
-                  omptEvent->record.new_task.codeptr_ofn =
-                     ( void * ) event.getWD()->getActiveDevice().getWorkFct();
-                  break;
-               case TaskEnd:
-                  omptEvent->record.task.task_id = wd->getId();
-                  break;
-               case TaskSwitch:
-                  omptEvent->record.task_switch.first_task_id =
-                     ( wd != NULL ) ? wd->getId() : 0;
-                  omptEvent->record.task_switch.second_task_id =
-                     ( auxWD != NULL ) ? auxWD->getId() : 0;
-                  break;
-            }
-
-            buffer.done++;
          }
          void createEventBuffer() {
              _devEventBuffers.push_back( NEW BufferInfo() );
@@ -954,7 +962,7 @@ extern "C" {
        * this type signature
        */
       InstrumentationOMPT *instr = ( InstrumentationOMPT *) sys.getInstrumentation();
-      DeviceInstrumentation *devInstr = sys.getDeviceInstrumentation( device_id );
+      DeviceInstrumentation *devInstr = instr->getDeviceInstrumentation( device_id );
       *type = devInstr->getDeviceType();
       *device = (ompt_target_device_t*)devInstr;
       *lookup = ompt_nanos_target_lookup;

src/arch/fpga/fpgainstrumentation.cpp
@@ -5,15 +5,15 @@
 using namespace nanos;
 using namespace nanos::ext;
 
-unsigned long long int FPGAInstrumentation::getDeviceTime() const {
+nanos_event_time_t FPGAInstrumentation::getDeviceTime() const {
    uint64_t time;
    //TODO error checking
    xdmaGetDeviceTime(&time);
    debug0( "Init device time "  << time );
-   return (unsigned long long)time;
+   return ( nanos_event_time_t )( time );
 }
 
-unsigned long long FPGAInstrumentation::translateDeviceTime( unsigned long long devTime ) const {
+nanos_event_time_t FPGAInstrumentation::translateDeviceTime( nanos_event_time_t devTime ) const {
    //devTime is raw device time in cycles
    //_deviceInfo->getFreq() returns Mhz (10^6 cycles/sec == 1 cycle/us)
    return devTime * 1000 / _deviceInfo->getFreq();

src/arch/fpga/fpgainstrumentation.hpp
@@ -32,14 +32,14 @@ namespace ext {
       public:
          FPGAInstrumentation() : DeviceInstrumentation(), _deviceType( "NULL" ), _deviceInfo( NULL ) { }
 
-         FPGAInstrumentation( int id, std::string deviceType, FPGAProcessorInfo const * const fpgaInfo ) :
-            DeviceInstrumentation( id ), _deviceType( deviceType ), _deviceInfo( fpgaInfo ) { }
+         FPGAInstrumentation( std::string deviceType, FPGAProcessorInfo const * const fpgaInfo ) :
+            DeviceInstrumentation(), _deviceType( deviceType ), _deviceInfo( fpgaInfo ) { }
 
          virtual void init() {}
          //! \breif Returns the device time in cycles
-         virtual unsigned long long int getDeviceTime() const;
-         //! \brief Translates a raw device time in cycles to ns
-         virtual unsigned long long int translateDeviceTime( unsigned long long int ) const;
+         virtual nanos_event_time_t getDeviceTime() const;
+         //! \brief Translates a raw device time (in cycles) to ns
+         virtual nanos_event_time_t translateDeviceTime( const nanos_event_time_t devTime ) const;
          virtual void startDeviceTrace() {}
          virtual void pauseDeviceTrace( bool pause ) {}
          virtual void stopDeviceTrace() {}

src/arch/fpga/fpgaprocessor.cpp
@@ -55,24 +55,9 @@ FPGAProcessor::FPGAProcessor( FPGAProcessorInfo info, memory_space_id_t memSpace
 {
 #ifdef NANOS_INSTRUMENTATION_ENABLED
    if ( !FPGAConfig::isInstrDisabled() ) {
-      int id;
       std::string devNum = toString( _fpgaProcessorInfo.getId() );
-
-      id = sys.getNumInstrumentAccelerators();
-      _devInstr = FPGAInstrumentation( id, std::string( "FPGA accelerator " ) + devNum, &_fpgaProcessorInfo );
+      _devInstr = FPGAInstrumentation( std::string( "FPGA accelerator " ) + devNum, &_fpgaProcessorInfo );
       sys.addDeviceInstrumentation( &_devInstr );
-
-      id = sys.getNumInstrumentAccelerators();
-      _dmaInInstr = FPGAInstrumentation( id, std::string( "DMA in " ) + devNum, &_fpgaProcessorInfo );
-      sys.addDeviceInstrumentation( &_dmaInInstr );
-
-      id = sys.getNumInstrumentAccelerators();
-      _dmaOutInstr = FPGAInstrumentation( id, std::string( "DMA out " ) + devNum, &_fpgaProcessorInfo );
-      sys.addDeviceInstrumentation( &_dmaOutInstr );
-
-      id = sys.getNumInstrumentAccelerators();
-      _submitInstrumentation = FPGAInstrumentation( id, std::string( "DMA submit " ) + devNum, &_fpgaProcessorInfo );
-      sys.addDeviceInstrumentation( &_submitInstrumentation );
    }
 #endif
 }
@@ -103,53 +88,6 @@ BaseThread & FPGAProcessor::createThread ( WorkDescriptor &helper, SMPMultiThrea
    FPGAThread  &th = *NEW FPGAThread( helper, this, parent );
    return th;
 }
-
-#ifdef NANOS_INSTRUMENTATION_ENABLED
-void FPGAProcessor::dmaSubmitStart( const WD *wd )
-{
-   if ( FPGAConfig::isInstrDisabled() ) return;
-
-   uint64_t timestamp;
-   xdma_status status;
-   status = xdmaGetDeviceTime( &timestamp );
-   if ( status != XDMA_SUCCESS ) {
-      if ( !_dmaSubmitWarnShown ) {
-         warning("Could not read accelerator " << getAccelId() <<
-                 " clock (dma submit start). [Warning only shown once]");
-         _dmaSubmitWarnShown = true;
-      }
-   } else {
-      Instrumentation *instr = sys.getInstrumentation();
-      instr->addDeviceEvent( Instrumentation::DeviceEvent(
-         timestamp, TaskBegin, &_submitInstrumentation, wd ) );
-      instr->addDeviceEvent( Instrumentation::DeviceEvent(
-         timestamp, TaskSwitch, &_submitInstrumentation, NULL, wd ) );
-   }
-}
-
-void FPGAProcessor::dmaSubmitEnd( const WD *wd )
-{
-   if ( FPGAConfig::isInstrDisabled() ) return;
-
-   uint64_t timestamp;
-   xdma_status status;
-   status = xdmaGetDeviceTime( &timestamp );
-   if ( status != XDMA_SUCCESS ) {
-      if ( !_dmaSubmitWarnShown ) {
-         warning("Could not read accelerator " << getAccelId() <<
-                 " clock (dma submit end). [Warning only shown once]");
-         _dmaSubmitWarnShown = true;
-      }
-   } else {
-      Instrumentation *instr = sys.getInstrumentation();
-      //FIXME: Emit the accelerator ID
-      instr->addDeviceEvent( Instrumentation::DeviceEvent(
-         timestamp, TaskSwitch, &_submitInstrumentation, wd, NULL ) );
-      instr->addDeviceEvent( Instrumentation::DeviceEvent(
-         timestamp, TaskEnd, &_submitInstrumentation, wd ) );
-   }
-}
-#endif  //NANOS_INSTRUMENTATION_ENABLED
 
 xtasks_task_handle FPGAProcessor::createAndSubmitTask( WD &wd ) {
    xtasks_stat status;
@@ -184,16 +122,11 @@ xtasks_task_handle FPGAProcessor::createAndSubmitTask( WD &wd ) {
       ensure( status == XTASKS_SUCCESS, "Error adding arguments to a FPGA task" );
    }
 
-#ifdef NANOS_INSTRUMENTATION_ENABLED
-   dmaSubmitStart( &wd );
-#endif
    if ( xtasksSubmitTask( task ) != XTASKS_SUCCESS ) {
       //TODO: If error is XDMA_ENOMEM we can retry after a while
       fatal("Error sending a task to the FPGA");
    }
-#ifdef NANOS_INSTRUMENTATION_ENABLED
-   dmaSubmitEnd( &wd );
-#endif
+
    return task;
 }
 
@@ -201,41 +134,20 @@ xtasks_task_handle FPGAProcessor::createAndSubmitTask( WD &wd ) {
 void FPGAProcessor::readInstrCounters( WD * const wd, xtasks_task_handle & task ) {
    if ( FPGAConfig::isInstrDisabled() ) return;
 
-   Instrumentation * instr = sys.getInstrumentation();
+   static Instrumentation * ins     = sys.getInstrumentation();
+   static nanos_event_key_t inKey   = ins->getInstrumentationDictionary()->getEventKey( "device-copy-in" );
+   static nanos_event_key_t execKey = ins->getInstrumentationDictionary()->getEventKey( "device-task-execution" );
+   static nanos_event_key_t outKey  = ins->getInstrumentationDictionary()->getEventKey( "device-copy-out" );
+   nanos_event_value_t val = ( nanos_event_value_t )( wd->getId() );
 
    //Get the counters
    xtasks_ins_times * counters;
    xtasksGetInstrumentData( task, &counters );
 
-   //Task execution
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->inTransfer, TaskBegin, &_devInstr, wd ) );
-   //Beginning kernel execution is represented as a task switch from NULL to a WD
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->inTransfer, TaskSwitch, &_devInstr, NULL, wd ) );
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->computation, TaskSwitch, &_devInstr, wd, NULL ) );
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->computation, TaskEnd, &_devInstr, wd ) );
-
-   //DMA info
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->start, TaskBegin, &_dmaInInstr, wd ) );
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->start, TaskSwitch, &_dmaInInstr, NULL, wd ) );
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->inTransfer, TaskSwitch, &_dmaInInstr, wd, NULL ) );
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->inTransfer, TaskEnd, &_dmaInInstr, wd ) );
-
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->computation, TaskBegin, &_dmaOutInstr, wd ) );
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->computation, TaskSwitch, &_dmaOutInstr, NULL, wd ) );
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->outTransfer, TaskSwitch, &_dmaOutInstr, wd, NULL ) );
-   instr->addDeviceEvent(
-         Instrumentation::DeviceEvent( counters->outTransfer, TaskEnd, &_dmaOutInstr, wd ) );
+   //Raise the events
+   ins->raiseDeviceBurstEvent( _devInstr, inKey,   val, counters->start,       counters->inTransfer  );
+   ins->raiseDeviceBurstEvent( _devInstr, execKey, val, counters->inTransfer,  counters->computation );
+   ins->raiseDeviceBurstEvent( _devInstr, outKey,  val, counters->computation, counters->outTransfer );
 }
 #endif
 

src/arch/fpga/fpgaprocessor.hpp
@@ -52,9 +52,6 @@ namespace ext {
 
 #ifdef NANOS_INSTRUMENTATION_ENABLED
          FPGAInstrumentation           _devInstr;
-         FPGAInstrumentation           _dmaInInstr;
-         FPGAInstrumentation           _dmaOutInstr;
-         FPGAInstrumentation           _submitInstrumentation;
          bool                          _dmaSubmitWarnShown; //!< Defines if the warning has already been shown
          static Atomic<size_t>         _totalRunningTasks;  //!< Global tasks counter between all processors
 #endif
@@ -62,8 +59,6 @@ namespace ext {
          // AUX functions
          xtasks_task_handle createAndSubmitTask( WD &wd );
 #ifdef NANOS_INSTRUMENTATION_ENABLED
-         void dmaSubmitStart( const WD *wd );
-         void dmaSubmitEnd( const WD *wd );
          void readInstrCounters( WD * const wd, xtasks_task_handle & task );
 #endif
 

commit 33727e2302f40dab521e12da27ebe6f45f27acbe

Fix OMPT instrumentation to show device events
Broken in commit c44d828a687cde31b5503a912f2d8b5352cdc813

src/plugins/instrumentation/ompt.cpp
@@ -507,29 +507,6 @@ namespace nanos
 
          std::vector < BufferInfo* > _devEventBuffers;
 
-         //! \brief Translates the nanos_event_type_t to ompt_event_t
-         static ompt_event_t getOMPTEventType( nanos_event_type_t type ) {
-            ompt_event_t ret = ( ompt_event_t )( 0 );
-            switch ( type ) {
-               case NANOS_STATE_START:
-               case NANOS_STATE_END:
-               case NANOS_SUBSTATE_START:
-               case NANOS_SUBSTATE_END:
-               case NANOS_PTP_START:
-               case NANOS_PTP_END:
-               case NANOS_POINT:
-                  break; //< Previous event types are not supported yet
-               case NANOS_BURST_START:
-                  ret = ompt_event_task_begin;
-                  break;
-               case NANOS_BURST_END:
-                  ret = ompt_event_task_end;
-                  break;
-               default: break;
-            }
-            return ret;
-         }
-
       public:
          InstrumentationOMPT( ) : Instrumentation( *NEW InstrumentationContextDisabled()),
             _previousTask( NULL ), _threadActive( NULL ), _requestBufferCallback( NULL ),
@@ -869,6 +846,7 @@ namespace nanos
             int deviceId = ctx.getId();
             BufferInfo &buffer = *_devEventBuffers[ deviceId ];
             unsigned int eventIdx = 0; //< Index being handled of events array
+            ompt_thread_id_t threadId = ( ompt_thread_id_t )( getCurrentThreadId() );
 
             while ( eventIdx < count ) {
                buffer.lock++; //Lock the buffer to get an empty slot
@@ -896,32 +874,64 @@ namespace nanos
                   buffer.current = buffer.begin;
                }
                unsigned int current = buffer.current; //< Initial position to write in the buffer
-               unsigned int countCurrent = std::min( count - eventIdx, buffer.records - current );
+               //! NOTE: Each input event generate two OMPT events:
+               //!        - NANOS_BURST_START: ompt_event_task_begin + ompt_event_task_switch
+               //!        - NANOS_BURST_END: ompt_event_task_switch + ompt_event_task_end
+               unsigned int countCurrent = std::min( ( count - eventIdx )*2, buffer.records - current );
                buffer.current += countCurrent;
                buffer.lock--; //Release the lock and use local variable
 
-               for (unsigned int i = 0; i < countCurrent; ++i) {
+               for (unsigned int i = 0; i < countCurrent; i += 2) {
                   //add event to buffer
-                  ompt_record_ompt_t *omptEvent = &buffer.buffer[ current + i ];
-                  //Nanos device events are already conveniently set to match ompt spec
-                  omptEvent->type = ompt_event_task_switch; //getOMPTEventType( events[eventIdx].getType() );
-                  omptEvent->time = events[eventIdx].getDeviceTime();
-                  //Host thread that emits the event (TODO check)
-                  omptEvent->thread_id = ( ompt_thread_id_t )( getCurrentThreadId() );
-                  omptEvent->dev_task_id = events[eventIdx].getValue(); //< Event value should be the taskID
-
+                  nanos_event_value_t eventValue = events[eventIdx].getValue();
+                  nanos_event_time_t eventTime = events[eventIdx].getDeviceTime();
                   switch ( events[eventIdx].getType() ) {
-                     case NANOS_BURST_START:
-                        omptEvent->record.task_switch.first_task_id = 0;
-                        omptEvent->record.task_switch.second_task_id = events[eventIdx].getValue();
+                     case NANOS_BURST_START: {
+                        //set the ompt_event_task_begin information
+                        ompt_record_ompt_t *burstEvent = &buffer.buffer[ current + i ];
+                        burstEvent->type = ompt_event_task_begin;
+                        burstEvent->time = eventTime;
+                        burstEvent->thread_id = threadId;
+                        burstEvent->dev_task_id = eventValue; //< Event value should be the taskID
+                        burstEvent->record.new_task.parent_task_id = 1; //wd->getParent()->getId();
+                        burstEvent->record.new_task.parent_task_frame = NULL;
+                        burstEvent->record.new_task.new_task_id = eventValue;
+                        //NOTE: The codeptr_ofn field must be not NULL as Extrae stores a pair (task_id, codeptr) used to emit the task switches.
+                        //      Those switches will be ignored if the codeptr is NULL
+                        uintptr_t codeptr = ( uintptr_t )( 0xF0000000 + events[eventIdx].getKey() ); //wd->getActiveDevice().getWorkFct();
+                        burstEvent->record.new_task.codeptr_ofn = ( void * )( codeptr );
+
+                        //set the ompt_event_task_switch information
+                        ompt_record_ompt_t *switchEvent = burstEvent + 1;
+                        switchEvent->type = ompt_event_task_switch;
+                        switchEvent->time = eventTime;
+                        switchEvent->thread_id = threadId;
+                        switchEvent->dev_task_id = eventValue; //< Event value should be the taskID
+                        switchEvent->record.task_switch.first_task_id = 0;
+                        switchEvent->record.task_switch.second_task_id = eventValue;
                         break;
-                     case NANOS_BURST_END:
-                        omptEvent->record.task_switch.first_task_id = events[eventIdx].getValue();
-                        omptEvent->record.task_switch.second_task_id = 0;
+                     } case NANOS_BURST_END: {
+                        //set the ompt_event_task_switch information
+                        ompt_record_ompt_t *switchEvent = &buffer.buffer[ current + i ];
+                        switchEvent->type = ompt_event_task_switch;
+                        switchEvent->time = eventTime;
+                        switchEvent->thread_id = threadId;
+                        switchEvent->dev_task_id = eventValue; //< Event value should be the taskID
+                        switchEvent->record.task_switch.first_task_id = eventValue;
+                        switchEvent->record.task_switch.second_task_id = 0;
+
+                        //set the ompt_event_task_end information
+                        ompt_record_ompt_t *burstEvent = switchEvent + 1;
+                        burstEvent->type = ompt_event_task_end;
+                        burstEvent->time = eventTime;
+                        burstEvent->thread_id = threadId;
+                        burstEvent->dev_task_id = eventValue;
+                        burstEvent->record.task.task_id = eventValue;
                         break;
-                     default:
+                     } default: {
                         warning( "Found an unsupported event type in InstrumentationOMPT::addDeviceEventList" );
                         break;
+                     }
                   }
 
                   eventIdx++;