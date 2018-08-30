9ce97b01cf7afe819cba2af8f9060b9870898474
Jaume Bosch <jbosch@bsc.es>
2018-08-30 12:30:45 +0200

    Fix some EventKeys registration
    There was a missing boolean in some calls to registerEventKey

--- src/core/instrumentation_decl.hpp
+++ src/core/instrumentation_decl.hpp
@@ -689,13 +689,13 @@ namespace nanos {
             /* 73 */ registerEventKey("fpga-finish-task", "Finished FPGA task ID that is being processed", true, EVENT_ADVANCED);
             /* 74 */ registerEventKey("fpga-run-tasks", "Number of FPGA tasks submitted to the accels", true, EVENT_ADVANCED);
 
-            /* 75 */ registerEventKey("cluster-offload-task", "Task being offloaded to a remote node", EVENT_DEVELOPER);
-            /* 76 */ registerEventKey("cluster-select-node", "Remote node being selected for remote task offload", EVENT_DEVELOPER);
-            /* 77 */ registerEventKey("cluster-finish-wd", "Remote WD being finished", EVENT_DEVELOPER);
+            /* 75 */ registerEventKey("cluster-offload-task", "Task being offloaded to a remote node", true, EVENT_DEVELOPER);
+            /* 76 */ registerEventKey("cluster-select-node", "Remote node being selected for remote task offload", true, EVENT_DEVELOPER);
+            /* 77 */ registerEventKey("cluster-finish-wd", "Remote WD being finished", true, EVENT_DEVELOPER);
 
-            /* 78 */ registerEventKey("device-copy-in", "Device is copying from host the input data of a WD", EVENT_USER);
-            /* 79 */ registerEventKey("device-copy-out", "Device is copying to host the output data of a WD", EVENT_USER);
-            /* 80 */ registerEventKey("device-task-execution", "Device is executing a WD", EVENT_USER);
+            /* 78 */ registerEventKey("device-copy-in", "Device is copying from host the input data of a WD", true, EVENT_USER);
+            /* 79 */ registerEventKey("device-copy-out", "Device is copying to host the output data of a WD", true, EVENT_USER);
+            /* 80 */ registerEventKey("device-task-execution", "Device is executing a WD", true, EVENT_USER);
 
             /* ** */ registerEventKey("debug","Debug Key", true, EVENT_ADVANCED ); /* Keep this key as the last one */
          }
