5bfeaabac54b23a594127643c58cfc3c86044500
Jaume Bosch <jbosch@bsc.es>
2019-04-24 12:11:05 +0200

    Fix nanos API decl

--- src/arch/fpga/nanos-fpga.h
+++ src/arch/fpga/nanos-fpga.h
@@ -51,9 +51,11 @@ extern "C" {
         NANOS_ARGFLAG_NONE     = 0x00
     } nanos_fpga_argflag_t;
 
-    typedef struct {
+    typedef struct __attribute__ ((__packed__)) {
         uint64_t address;
-        uint32_t flags;
+        uint8_t  flags;
+        uint8_t  arg_idx;
+        uint16_t _padding;
         uint32_t size;
         uint32_t offset;
         uint32_t accessed_length;
@@ -69,12 +71,15 @@ NANOS_API_DECL( void *, nanos_fpga_alloc_dma_mem, ( size_t len) );
 NANOS_API_DECL( void, nanos_fpga_free_dma_mem, ( void * address ) );
 NANOS_API_DECL( nanos_err_t, nanos_find_fpga_pe, ( void *req, nanos_pe_t * pe ) );
 NANOS_API_DECL( void *, nanos_fpga_get_phy_address, ( void * address ) );
-NANOS_API_DECL( nanos_err_t, nanos_fpga_set_task_arg, ( nanos_wd_t wd, size_t argIdx, bool isInput, bool isOutput, uint64_t argValue ) );
+NANOS_API_DECL( nanos_err_t, nanos_fpga_set_task_arg, ( nanos_wd_t wd, size_t argIdx, \
+   bool isInput, bool isOutput, uint64_t argValue ) );
 NANOS_API_DECL( void *, nanos_fpga_malloc, ( size_t len ) );
 NANOS_API_DECL( void, nanos_fpga_free, ( void * fpgaPtr ) );
-NANOS_API_DECL( void, nanos_fpga_memcpy, ( void * fpgaPtr, void * hostPtr, size_t len, nanos_fpga_memcpy_kind_t kind ) );
-NANOS_API_DECL( void, nanos_fpga_create_wd_async, ( uint32_t archMask, uint64_t type, uint16_t numDeps, uint16_t numArgs, \
-  uint64_t * args, uint8_t * argsFlags, uint16_t numCopies, nanos_fpga_copyinfo_t * copies ) );
+NANOS_API_DECL( void, nanos_fpga_memcpy, ( void * fpgaPtr, void * hostPtr, size_t len, \
+   nanos_fpga_memcpy_kind_t kind ) );
+NANOS_API_DECL( void, nanos_fpga_create_wd_async, ( uint32_t archMask, uint64_t type, \
+  uint8_t numArgs, uint64_t * args, uint8_t numDeps, uint64_t * deps, uint8_t * depsFlags, \
+  uint8_t numCopies, nanos_fpga_copyinfo_t * copies ) );
 NANOS_API_DECL( nanos_err_t, nanos_fpga_register_wd_info, ( uint64_t type, size_t num_devices, \
   nanos_device_t * devices, nanos_translate_args_t translate ) );
 
