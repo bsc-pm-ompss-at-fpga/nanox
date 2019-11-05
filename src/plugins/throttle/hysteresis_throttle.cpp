bsc-pm-ompss-at-fpga/nanox

commit 4a3566cecd2a5d0895f77622591880b4f6275258

Add testThrottleIn to check throttle status whithout block

src/plugins/throttle/hysteresis_throttle.cpp
@@ -68,6 +68,7 @@ namespace nanos {
 
             bool throttleIn( void );
             void throttleOut ( void );
+            bool testThrottleIn( void );
 
             ~HysteresisThrottle() {
                delete _syncCond;
@@ -76,12 +77,18 @@ namespace nanos {
 
       bool HysteresisThrottle::throttleIn ( void )
       {
+         if ( testThrottleIn() ) _syncCond->wait();
+         return true;
+      }
+
+      bool HysteresisThrottle::testThrottleIn ( void )
+      {
          // If it's OpenMP, first level tasks will have depth 1
          unsigned maxDepth = ( sys.getPMInterface().getInterface() == PMInterface::OpenMP ) ? 2 : 1;
          // Only dealing with first level tasks
-         if ( ( (myThread->getCurrentWD())->getDepth() < maxDepth ) && ( _get_num_tasks() > _upper ) ) _syncCond->wait();
-         return true;
+         return ( ( (myThread->getCurrentWD())->getDepth() < maxDepth ) && ( _get_num_tasks() > _upper ) ) ? false : true;
       }
+
       void HysteresisThrottle::throttleOut ( void )
       {
          if ( _get_num_tasks() <= _lower ) _syncCond->signal();
@@ -117,7 +124,7 @@ namespace nanos {
             }
 
             virtual void init() {
-               sys.setThrottlePolicy( NEW HysteresisThrottle( _upperLimit, _lowerLimit, _type ) ); 
+               sys.setThrottlePolicy( NEW HysteresisThrottle( _upperLimit, _lowerLimit, _type ) );
             }
       };
 

src/arch/fpga/fpgalistener.cpp
@@ -72,13 +72,12 @@ void FPGACreateWDListener::callback( BaseThread* self )
       NANOS_INSTRUMENT( InstrumentBurst instBurst( "fpga-listener", "create-wd" ) );
 
       xtasks_newtask *task = NULL;
-      while ( sys.throttleTaskIn() ) {
+      while ( sys.testThrottleTaskIn() ) {
          if ( xtasksTryGetNewTask( &task ) != XTASKS_SUCCESS ) {
-            //NOTE: Keep the throttle policy coherent as we finally did not created a task
-            sys.throttleTaskOut();
             free(task);
             break;
          }
+         sys.throttleTaskIn(); //TODO: If this call returns false, the thread should block at this point
 
          FPGARegisteredTasksMap::const_iterator infoIt = _registeredTasks->find( task->typeInfo );
          ensure( infoIt != _registeredTasks->end(), " FPGA device trying to create an unregistered task" );

src/core/system.hpp
@@ -432,6 +432,7 @@ inline PinnedAllocator& System::getPinnedAllocatorCUDA() { return _pinnedMemoryC
 
 inline bool System::throttleTaskIn ( void ) const { return _throttlePolicy->throttleIn(); }
 inline void System::throttleTaskOut ( void ) const { _throttlePolicy->throttleOut(); }
+inline bool System::testThrottleTaskIn ( void ) const { return _throttlePolicy->testThrottleIn(); }
 
 inline void System::threadReady()
 {

src/core/system_decl.hpp
@@ -479,6 +479,7 @@ namespace nanos {
 
          bool throttleTaskIn( void ) const;
          void throttleTaskOut( void ) const;
+         bool testThrottleTaskIn( void ) const;
 
          const std::string & getDefaultSchedule() const;
 

src/core/throttle_decl.hpp
@@ -40,6 +40,10 @@ namespace nanos {
 
          virtual bool throttleIn( void )  = 0 ;
          virtual void throttleOut( void ) { /* empty function */ }
+
+         /*! \brief Test (it will not block) the expected result of calling throttleIn
+          */
+         virtual bool testThrottleIn( void ) { return throttleIn(); }
    };
 } // namespace nanos
 

commit 8c382358c3c0ae4f609f534554c38166c985f623

Fix blocking condition of hysteresis_throttle
Issue introduced in commit 4a3566ce. It was not detected before becasue the first thing that a wait do is check the condition in the propper way. However, it was limiting the number of on-the-fly tasks to _lower limit instread of _upper

src/plugins/throttle/hysteresis_throttle.cpp
@@ -77,7 +77,7 @@ namespace nanos {
 
       bool HysteresisThrottle::throttleIn ( void )
       {
-         if ( testThrottleIn() ) _syncCond->wait();
+         if ( !testThrottleIn() ) _syncCond->wait();
          return true;
       }
 