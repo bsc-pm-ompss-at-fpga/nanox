bsc-pm-ompss-at-fpga/nanox

commit fcfaabd97b3fef7adb1b58a19a138b8858e45598

Unregister memory regions during finalization of FPGA created tasks
Otherwise the folowing task that re-use the same memory storage may collison causing an invalid key fatal error

src/core/memcontroller.cpp
@@ -561,7 +561,7 @@ void MemController::setMainWD() {
 
 
 void MemController::synchronize( std::size_t numDataAccesses, DataAccess *data ) {
-   sys.getHostMemory().synchronize( *_wd, numDataAccesses, data );
+   sys.getHostMemory().synchronize( *_wd, numDataAccesses );
 }
 void MemController::synchronize() {
    sys.getHostMemory().synchronize( *_wd );

src/arch/fpga/fpgawd.cpp
@@ -34,15 +34,21 @@ FPGAWD::FPGAWD ( int ndevices, DeviceData **devs, size_t data_size, size_t data_
 {}
 
 void FPGAWD::notifyParent() {
-   //FIXME: The copy data may not be in copies[].address as it may be moved into another address space.
-   //       In addition, the directory entry for copies[].address must be removed as it probably will
-   //       be used again in a future task.
-
    NANOS_INSTRUMENT( InstrumentBurst instBurst( "fpga-notify-task", getParent()->getId() ) );
+   //FIXME: The current WD may not have a parent task
 
    //Copy the data back to the FPGA memory before doing the notification
+   //NOTE: First the WD cache must be updated to get the data back into the host addressspace
    CopyData const * copies = getCopies();
-   for ( size_t cIdx = 0; cIdx < getNumCopies(); ++cIdx ) {
+   const size_t numCopies = getNumCopies();
+   DataAccess * regions = ( DataAccess * )( calloc( numCopies, sizeof( DataAccess ) ) );
+   for ( size_t cIdx = 0; cIdx < numCopies; ++cIdx ) {
+      //NOTE: Only setting the used fields
+      new (regions + cIdx) DataAccess( copies[cIdx].address, copies[cIdx].flags.input, copies[cIdx].flags.output,
+         0 /*canRename*/, 0 /*concurrent*/, 0 /*commutative*/, 0 /*dimCount*/, NULL, 0 );
+   }
+   sys.getHostMemory().synchronize( *getParent(), numCopies, regions, true /*forceUnregister*/ );
+   for ( size_t cIdx = 0; cIdx < numCopies; ++cIdx ) {
       void * hostAddr = copies[cIdx].address;
       if ( copies[cIdx].flags.output ) {
          const uint64_t devAddr = _origFpgaCopiesAddrs[cIdx];

src/core/addressspace.cpp
@@ -44,16 +44,16 @@ void HostAddressSpace::failToLock( SeparateMemoryAddressSpace &from, global_reg_
    std::cerr << __FUNCTION__ << " @ " << __FILE__ << " : " << __LINE__ << " unimplemented" << std::endl;
 }
 
-void HostAddressSpace::synchronize( WD &wd, std::size_t numDataAccesses, DataAccess *data ) {
-   _directory.synchronize( wd, numDataAccesses, data ); //needs wd
+void HostAddressSpace::synchronize( WD &wd, std::size_t numDataAccesses, DataAccess *data, const bool forceUnregister ) {
+   _directory.synchronize( wd, numDataAccesses, data, forceUnregister ); //needs wd
 }
 
-void HostAddressSpace::synchronize( WD &wd, void *addr ) {
-   _directory.synchronize( wd, addr ); //needs wd
+void HostAddressSpace::synchronize( WD &wd, void *addr, const bool forceUnregister ) {
+   _directory.synchronize( wd, addr, forceUnregister ); //needs wd
 }
 
-void HostAddressSpace::synchronize( WD &wd ) {
-   _directory.synchronize( wd );
+void HostAddressSpace::synchronize( WD &wd, const bool forceUnregister ) {
+   _directory.synchronize( wd, forceUnregister );
 }
 
 memory_space_id_t HostAddressSpace::getMemorySpaceId() const {

src/core/addressspace_decl.hpp
@@ -53,7 +53,7 @@ inline TransferListEntry::TransferListEntry( global_reg_t reg, unsigned int vers
    _reg( reg ), _version( version ), _ops( ops ), _destinationChunk( destinationChunk ), _sourceChunk( sourceChunk ), _srcAddress( srcAddr ), _copyIndex( copyIdx ) {
 }
 
-inline TransferListEntry::TransferListEntry( TransferListEntry const &t ) : 
+inline TransferListEntry::TransferListEntry( TransferListEntry const &t ) :
    _reg( t._reg ), _version( t._version ), _ops( t._ops ), _destinationChunk( t._destinationChunk ), _sourceChunk( t._sourceChunk ), _srcAddress( t._srcAddress ), _copyIndex( t._copyIndex ) {
 }
 
@@ -109,9 +109,9 @@ class HostAddressSpace {
    void getRegion( CopyData const &cd, global_reg_t &reg, WD const *wd );
    void failToLock( MemSpace< SeparateAddressSpace > &from, global_reg_t const &reg, unsigned int version );
 
-   void synchronize( WD &wd );
-   void synchronize( WD &wd, void *addr );
-   void synchronize( WD &wd, std::size_t numDataAccesses, DataAccess *data );
+   void synchronize( WD &wd, const bool forceUnregister = false );
+   void synchronize( WD &wd, void *addr, const bool forceUnregister = false );
+   void synchronize( WD &wd, std::size_t numDataAccesses, DataAccess *data, const bool forceUnregister = false );
    memory_space_id_t getMemorySpaceId() const;
    reg_t getLocalRegionId( void *hostObject, reg_t hostRegionId );
    RegionDirectory::RegionDirectoryKey getRegionDirectoryKey( uint64_t addr );
@@ -127,7 +127,7 @@ class SeparateAddressSpace {
    unsigned int _acceleratorNumber;
    bool         _isAccelerator;
    void        *_sdata;
-   
+
    public:
    SeparateAddressSpace( memory_space_id_t memorySpaceId, Device &arch, bool allocWide, std::size_t slabSize );
 
@@ -141,7 +141,7 @@ class SeparateAddressSpace {
    void releaseRegions( MemCacheCopy *memCopies, unsigned int numCopies, WD const &wd );
    //void releaseRegion( global_reg_t const &reg, WD const &wd, unsigned int copyIdx, enum RegionCache::CachePolicy policy );
    uint64_t getDeviceAddress( global_reg_t const &reg, uint64_t baseAddress, AllocatedChunk *chunk ) const;
-   
+
    bool prepareRegions( MemCacheCopy *memCopies, unsigned int numCopies, WD const &wd );
    void setRegionVersion( global_reg_t const &reg, AllocatedChunk *chunk, unsigned int version, WD const &wd, unsigned int copyIdx );
    unsigned int getCurrentVersion( global_reg_t const &reg, WD const &wd, unsigned int copyIdx );

src/core/regiondirectory.cpp
@@ -359,7 +359,7 @@ void RegionDirectory::_unregisterObjects( std::map< uint64_t, MemoryMap< Object 
    }
 }
 
-void RegionDirectory::synchronize( WD &wd, void *addr ) {
+void RegionDirectory::synchronize( WD &wd, void *addr, const bool forceUnregister ) {
    //std::ostream &o = (*myThread->_file);
    //o << "++++ WaitOn synchronize, w addr " << addr << std::endl;
    uint64_t objectAddr = (uint64_t) addr;
@@ -448,7 +448,7 @@ void RegionDirectory::synchronize( WD &wd, void *addr ) {
    while ( !outOps.isDataReady( wd ) ) { myThread->processTransfers(); }
    //sys.setVerboseDevOps( orig_verbose_devops );
 
-   if ( wd.getDepth() == 0 ) {
+   if ( wd.getDepth() == 0 || forceUnregister ) {
       // invalidate data on devices
       _invalidateObjectsFromDevices( objects_to_clear );
 
@@ -463,7 +463,7 @@ void RegionDirectory::synchronize( WD &wd, void *addr ) {
    //o << "++++ DONE ++++ WaitOn synchronize, w addr " << addr << std::endl;
 }
 
-void RegionDirectory::synchronize( WD &wd, std::size_t numDataAccesses, DataAccess *data )
+void RegionDirectory::synchronize( WD &wd, std::size_t numDataAccesses, DataAccess *data, const bool forceUnregister )
 {
    SeparateAddressSpaceOutOps outOps( myThread->runningOn(), true, false );
 
@@ -540,7 +540,7 @@ void RegionDirectory::synchronize( WD &wd, std::size_t numDataAccesses, DataAcce
    outOps.issue( &wd );
    while ( !outOps.isDataReady( wd ) ) { myThread->processTransfers(); }
 
-   if ( wd.getDepth() == 0 ) {
+   if ( wd.getDepth() == 0 || forceUnregister ) {
       // invalidate data on devices
       _invalidateObjectsFromDevices( objects_to_clear );
 
@@ -554,7 +554,7 @@ void RegionDirectory::synchronize( WD &wd, std::size_t numDataAccesses, DataAcce
    }
 }
 
-void RegionDirectory::synchronize( WD &wd ) {
+void RegionDirectory::synchronize( WD &wd, const bool forceUnregister ) {
    std::ostream &o = (*myThread->_file);
    //o << "++++ WaitOn synchronize ALL" << std::endl;
    //*myThread->_file << "SYNC DIR with wd " << wd.getId() << std::endl;
@@ -612,7 +612,7 @@ void RegionDirectory::synchronize( WD &wd ) {
          hb._lock.release();
       }
 
-      if ( wd.getDepth() == 0 ) {
+      if ( wd.getDepth() == 0 || forceUnregister ) {
          _unregisterObjects( objects_to_clear );
       }
       return;

src/core/regiondirectory_decl.hpp
@@ -75,7 +75,7 @@ namespace nanos {
   /*! \class NewDirectory
    *  \brief Stores copy accesses controls their versions and if they are dirty in any cache
    */
-   typedef std::list< std::pair< reg_t, reg_t > > NewLocationInfoList; 
+   typedef std::list< std::pair< reg_t, reg_t > > NewLocationInfoList;
    class RegionDirectory
    {
       private:
@@ -144,11 +144,11 @@ namespace nanos {
 
       private:
 
-         /*! \brief NewDirectory copy constructor (private) 
+         /*! \brief NewDirectory copy constructor (private)
           */
          RegionDirectory( const RegionDirectory &dir );
 
-         /*! \brief NewDirectory copy assignment operator (private) 
+         /*! \brief NewDirectory copy assignment operator (private)
           */
          const RegionDirectory & operator= ( const RegionDirectory &dir );
 
@@ -167,9 +167,9 @@ namespace nanos {
          RegionDirectoryKey getRegionDirectoryKey( CopyData const &cd );
          RegionDirectoryKey getRegionDirectoryKey( uint64_t addr );
          RegionDirectoryKey getRegionDirectoryKeyRegisterIfNeeded( CopyData const &cd, WD const *wd );
-         void synchronize( WD &wd );
-         void synchronize( WD &wd, void *addr );
-         void synchronize( WD &wd, std::size_t numDataAccesses, DataAccess *data );
+         void synchronize( WD &wd, const bool forceUnregister = false );
+         void synchronize( WD &wd, void *addr, const bool forceUnregister = false );
+         void synchronize( WD &wd, std::size_t numDataAccesses, DataAccess *data, const bool forceUnregister = false );
 
          /*! \brief NewDirectory default constructor
           */

commit e314de3d3da983d8977617afdd9647c03bbe542b

Fix HostAddressSpace::synchronize call at WD::waitOn
Commit fcfaabd97b3fef7adb1b58a19a138b8858e45598 introduced a silent bug that causes a full data synchronization when a taskwait on(X) API call is performed

src/core/memcontroller.cpp
@@ -561,7 +561,7 @@ void MemController::setMainWD() {
 
 
 void MemController::synchronize( std::size_t numDataAccesses, DataAccess *data ) {
-   sys.getHostMemory().synchronize( *_wd, numDataAccesses );
+   sys.getHostMemory().synchronize( *_wd, numDataAccesses, data );
 }
 void MemController::synchronize() {
    sys.getHostMemory().synchronize( *_wd );